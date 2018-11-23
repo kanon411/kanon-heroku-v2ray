ec68928f8ba6464a5b645b20b33c672d219609ac
kanon411 <kanon411@gmail.com>
2018-11-23 15:06:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -89,7 +89,7 @@ http://0.0.0.0:${PORT}
 	root /wwwroot
 	index index.html
 	timeouts none
-	proxy ${V2_Path} localhost:60000 {
+	proxy ${V2_Path} localhost:60007 {
 		websocket
 		header_upstream -Origin
 	}
